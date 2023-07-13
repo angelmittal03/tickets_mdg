49613f030f09f0a2fa68609a94f3d854ffb0801a
ANGEL <mittalangel003@gmail.com>
2023-07-14 03:35:35 +0530

    ft:Homepage added

---
+++ src/app/HomePage/page.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <body>
+    <div className=" bg-gradient-to-r from-orange-400 via-red-500 to-pink-500">
+ 
+
+      <main/>
+      </div>
+      </body>
+  )
+}
+
+
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,12 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Inter, Poppins, Playfair_Display } from 'next/font/google'
+import Navbar from './Components/Navbar/NavBar'
+import Footer from './Components/Footer/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
+const poppins = Poppins({ subsets: ['latin'], weight: ['300','500','700'] })
+const playfairDisplay = Playfair_Display({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +20,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={poppins.className}>
+        <div className="container">
+        <Navbar/>
+        {children}
+        <Footer />
+        </div>
+      </body>
     </html>
   )
 }
