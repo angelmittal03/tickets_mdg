8815da03fea2276ff8bcfc299f1708f2292a8f04
Aayushmaan <goforaayush@gmail.com>
2023-07-15 21:01:19 +0530

    bfix: fixed integration

--- frontend/src/app/Success/[tid]/page.jsx
+++ frontend/src/app/Success/[tid]/page.jsx
@@ -1,21 +1,34 @@
 'use client';
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 
 export default function Success(param){
     const tid= param['params'].tid
-    
+    const [somedata,setsomedata] = useState([])
     useEffect(()=>{
-        console.log(tid)
-          fetch("http://127.0.0.1:8000/payment/"+tid.toString()+'/', {
-            method: 'GET',
-            redirect: 'follow'
-          })
-            .then(response => {response.text()})
-            .then(result => {console.log(result)
-            })
-            .catch(error => console.log('error', error));
+      const fetchData = async ()=>{
+        try
+        {const url = "http://127.0.0.1:8000/payment/"+tid.toString()+'/'
+        console.log(url)
+        const response = await fetch(url)
+        const data= await response.json()
+        console.log(data)
+      setsomedata(data)}
+        catch(e){
+          console.log(e)
+        }
+      }
+      fetchData()
+        // console.log(tid)
+          // fetch("http://127.0.0.1:8000/payment/"+tid.toString()+'/', {
+          //   method: 'GET',
+          //   redirect: 'follow'
+          // })
+          //   .then(response => {response.text()})
+          //   .then(result => {console.log(result)
+          //   })
+          //   .catch(error => console.log('error', error));
     },[])
-    return <>hi</>
+    return <>{JSON.stringify(somedata)}</>
 }
